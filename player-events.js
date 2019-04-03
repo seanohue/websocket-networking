82d0c71a66e88a9745a10094067233d8df5d4f64
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2019-04-03 15:16:28 -0400

    Impl custom client logic

--- player-events.js
+++ player-events.js
@@ -1,71 +1,94 @@
 'use strict';
 
-module.exports = {
-  listeners: {
-    attributeUpdate: state => function () {
-      updateAttributes.call(this);
-    },
+module.exports = (srcPath) => {
+  return  {
+    listeners: {
+      attributeUpdate: state => function () {
+        updateAttributes.call(this);
+      },
 
-    login: state => function () {
-      this.socket.command('sendData', 'quests', this.questTracker.serialize().active);
+      spawn: state => function () {
+        this._updated = 0;
+        this.socket.command('sendData', 'quests', this.questTracker.serialize().active);
 
-      const effects = this.effects.entries().filter(effect => !effect.config.hidden).map(effect => effect.serialize());
-      this.socket.command('sendData', 'effects', effects);
+        updateEffects.call(this);
+        updateAttributes.call(this);
+      },
 
-      updateAttributes.call(this);
-    },
+      combatantAdded: state => function () {
+        updateTargets.call(this);
+      },
 
-    combatantAdded: state => function () {
-      updateTargets.call(this);
-    },
+      combatantRemoved: state => function () {
+        updateTargets.call(this);
+      },
 
-    combatantRemoved: state => function () {
-      updateTargets.call(this);
-    },
+      updateTick: state => function () {
+        this._updated++;
+        if (this._updated % 20 === 0) {
+          updateEffects.call(this);
+          this._updated = 0;
+        }
 
-    updateTick: state => function () {
-      const effects = this.effects.entries().filter(effect => !effect.config.hidden).map(effect => ({
+        if (!this.isInCombat()) {
+          return;
+        }
+
+        updateTargets.call(this);
+      },
+
+      effectAdded: state => function (eff) {
+        if (eff.config.hidden) return;
+        updateEffects.call(this);
+      },
+
+      effectRemoved: state => function () {
+        if (!this.effects.size) {
+          this.socket.command('sendData', 'effects', []);
+        }
+        updateEffects.call(this);
+      },
+
+      questProgress: state => function () {
+        this.socket.command('sendData', 'quests', this.questTracker.serialize().active);
+      },
+    }
+  };
+};
+
+function updateEffects() {
+  const effectsMap = Array.from(this.effects.entries())
+    .filter(effect => !effect.config.hidden);
+
+  if (effectsMap.length) {
+    const effects = effectsMap
+      .map(effect => ({
         name: effect.name,
         elapsed: effect.elapsed,
         remaining: effect.remaining,
         config: {
           duration: effect.config.duration
         }
-      }));
-
-      if (effects.length) {
-        this.socket.command('sendData', 'effects', effects);
-      }
-
-      if (!this.isInCombat()) {
-        return;
-      }
-
-      updateTargets.call(this);
-    },
-
-    effectRemoved: state => function () {
-      if (!this.effects.size) {
-        this.socket.command('sendData', 'effects', []);
-      }
-    },
-
-    questProgress: state => function () {
-      this.socket.command('sendData', 'quests', this.questTracker.serialize().active);
-    },
+    }));
+    this.socket.command('sendData', 'effects', effects);
   }
-};
+}
 
 function updateAttributes() {
-  // example of sending player data to a websocket client. This data is not sent to the default telnet socket
-  let attributes = {};
+  const attributes = {};
   for (const [name, attribute] of this.attributes) {
-    attributes[name] = {
+    const attrData = {
       current: this.getAttribute(name),
       max: this.getMaxAttribute(name),
     };
-  }
 
+    const type = ['health', 'energy', 'focus'].includes(name) ? 'pool' : 'stat';
+    attrData.type = type;
+    if (type === 'stat') {
+      attrData.base = this.getBaseAttribute(name);
+    }
+    attributes[name] = attrData;
+  }
   this.socket.command('sendData', 'attributes', attributes);
 }
 
